f7dc6e39b0a60f149217dacb472333e61a281f64
burakasikk <buraktugrulasik@gmail.com>
2025-12-09 21:40:48 +0300

    feat: tasarım tokenlarıyla tema sistemi oluşturma (Bölüm 2)

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
 import { useFonts } from 'expo-font';
-import { View, Text, StyleSheet } from 'react-native';
+import ProfileScreen from './screens/ProfileScreen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    'Montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
-    'Montserrat Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat': require('./assets/Montserrat-Regular.ttf'),
+    'Montserrat Bold': require('./assets/Montserrat-Bold.ttf'),
   });
 
   if (!fontsLoaded) {
     return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Fontlar Yüklendi!</Text>
-    </View>
-  );
+  return <ProfileScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontFamily: 'Montserrat Bold',
-    fontSize: 24,
-  },
-});
